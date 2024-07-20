2c24fcf1741667d6624c878605983f5633ed3740
Halhadus <eneshakans45@proton.me>
2024-07-20 17:46:26 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,4 @@
 cp -rf /adaptation-xiaomi-begonia/* /
 cp -rf /adaptation-xiaomi-begonia/etc/environment.d/environment /etc/environment
 systemctl enable batman modules ssh-fix pbhelper droidian-perf
-groupadd --gid 1013 android_media
-usermod -aG android_media droidian
+bash /adaptation-xiaomi-begonia/groups.sh
