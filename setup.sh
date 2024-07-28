88dd9106bc8c984880d6856d57bc659c59e0860f
Halhadus <eneshakans45@proton.me>
2024-07-29 02:47:46 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # Run as root!
 cp -rf /adaptation-xiaomi-begonia/* /
 cp -rf /adaptation-xiaomi-begonia/etc/environment.d/environment /etc/environment
+rm /etc/ofono/ril_subscription.conf
 systemctl disable droidian-fpd
 systemctl enable batman modules ssh-fix pbhelper droidian-perf brightness
 bash /adaptation-xiaomi-begonia/groups.sh
