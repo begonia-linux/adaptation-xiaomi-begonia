d98dc0f8e47bf72e2117b5419756e81a5530ed36
Halhadus <eneshakans45@proton.me>
2024-07-29 02:13:42 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
 # Run as root!
+
+# Plasma installation
+cp /adaptation-xiaomi-begonia/etc/apt/source.list.d/lindroid.list /etc/apt/source.list.d
+cp /adaptation-xiaomi-begonia/etc/apt/preferences.d/10-lindroid /etc/apt/preferences.d
+mkdir -p /tmp/run/systemd/resolve
+echo "nameserver 9.9.9.11" > /tmp/run/systemd/resolve/stub-resolv.conf
+mount --bind /tmp/run /run
+apt update
+apt install plasma-mobile{-phone} systemsettings -y
+umount /run
+rm -rf /tmp/run
+
+# Fix touchscreen step 1
+/sbin/setcap cap_dac_read_search+ep /usr/bin/startplasma-wayland
+/sbin/setcap cap_dac_read_search+ep /usr/bin/kwin_wayland
+/sbin/setcap cap_dac_read_search+ep /usr/bin/startplasmamobile
+
+# Adaptation files
 cp -rf /adaptation-xiaomi-begonia/* /
 cp -rf /adaptation-xiaomi-begonia/etc/environment.d/environment /etc/environment
-systemctl disable droidian-fpd
-systemctl enable batman modules ssh-fix pbhelper droidian-perf brightness
+systemctl disable droidian-fpd batman
+systemctl enable inputpermissions modules ssh-fix pbhelper droidian-perf brightness
 bash /adaptation-xiaomi-begonia/groups.sh
